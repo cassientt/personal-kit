a4e933c93f841d3865cad408c241a87ddf3e1534
nietingting <nietingting@kuaishou.com>
2022-05-05 22:54:10 +0800

    first commit

---
+++ src/main/java/com/ntt/jsonTools/JSONArrayUse.java
@@ -0,0 +1,42 @@
+package com.ntt.jsonTools;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+/**
+ * @author nietingting <yourname@kuaishou.com>
+ * Created on 2022-05-05
+ */
+
+public class JSONArrayUse {
+
+    public static void main(String[] args) {
+        //创建JSONArray对象
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(0,"aa");
+        jsonArray.add(1,"BB");
+        jsonArray.element("cc");
+        jsonArray.add("DD");
+        System.out.println("1：" + jsonArray);
+        //根据下标获取数据
+        System.out.println("2：" + jsonArray.get(0));
+
+        //根据下标设置数据
+        jsonArray.set(0,"AAA");
+        System.out.println("3：" + jsonArray);
+
+        //创建JSONObject
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", "lwc");
+        jsonObject.put("password", "123");
+        //把JSONObject放入到JSONArray中
+        jsonArray.add(jsonObject);
+        System.out.println("4：" + jsonArray);
+
+        //遍历
+        System.out.println("5：");
+        for(int i=0;i<jsonArray.size();i++){
+            System.out.print(jsonArray.get(i)+"\t");
+        }
+    }
+}
