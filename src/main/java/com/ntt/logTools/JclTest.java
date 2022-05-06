02af7b831ce2f6ca1569cb8b1026216e25faba26
nietingting <nietingting@kuaishou.com>
2022-05-06 22:59:56 +0800

    日志处理

--- src/main/java/com/ntt/logTools/JclTest.java
+++ src/main/java/com/ntt/logTools/JclTest.java
@@ -1,14 +1,23 @@
 package com.ntt.logTools;
 
+
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import sun.rmi.runtime.Log;
 
+// kat 底层是对loginfo的封装，log写日志到磁盘，
+//问题的原因包导错了
 
+public class JclTest {
+    private static final Log log = LogFactory.getLog(JclTest.class);
 
-public class Main {
     public static void main(String[] args) {
-        Log log = LogFactory.getLog(Main.class);
-        log.info("start...");
-        log.warn("end.");
+        String msg = "print log, current level: ";
+        log.trace(msg + "trace");
+        log.debug(msg + "debug");
+        log.info(msg + "info");
+        log.warn(msg + "warn");
+        log.error(msg + "error");
+        log.fatal(msg + "fatal");
+        System.out.println("打印成功");
     }
 }
